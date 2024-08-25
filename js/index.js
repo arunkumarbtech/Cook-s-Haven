9a8b33a64a419b7f4e04748f8c0b44881efbe36e
ASHEYYY13 <aswinsalomon4321@gmail.com>
2024-08-25 20:44:26 +0530

    feat: adding second slider and footer js second time

--- js/index.js
+++ js/index.js
@@ -115,3 +115,24 @@ for(const item of scrollcontainer) {
         item.scrollLeft += evt.deltaY;
  } );
 }
+
+document.addEventListener("DOMContentLoaded", () => {
+
+	// Use Intersection Observer to determine if objects are within the viewport
+	const observer = new IntersectionObserver(entries => {
+	  entries.forEach(entry => {
+		if (entry.isIntersecting) {
+		  entry.target.classList.add('in-view');
+		  return;
+		}
+		entry.target.classList.remove('in-view');
+	  });
+	});
+
+	// Get all the elements with the .animate class applied
+	const allAnimatedElements = document.querySelectorAll('.animate');
+
+	// Add the observer to each of those elements
+	allAnimatedElements.forEach((element) => observer.observe(element));
+
+});
