d4fe1bd71a0296ee4fa07521cfbbc23c715a7aa4
Arun Kumar <arunkumarcs040@gmail.com>
2024-09-01 20:07:14 +0530

    order page html removed

--- js/order.js
+++
@@ -1,7 +0,0 @@
-const scrollcontainer = document.querySelectorAll(".cart-list");
-for(const item of scrollcontainer) {
-    item.addEventListener('wheel',(evt)=> {
-        evt.preventDefault();
-        item.scrollLeft += evt.deltaY;
- } );
-}
--- js/orderpage.js
+++ js/orderpage.js
@@ -56,3 +56,12 @@ document.querySelectorAll('.gallery-container').forEach(container => {
     // Initialize button states
     updateButtons(gallery, rightButton, leftButton);
 });
+
+
+const scrollcontainer = document.querySelectorAll(".cart-list");
+for(const item of scrollcontainer) {
+    item.addEventListener('wheel',(evt)=> {
+        evt.preventDefault();
+        item.scrollLeft += evt.deltaY;
+ } );
+}
