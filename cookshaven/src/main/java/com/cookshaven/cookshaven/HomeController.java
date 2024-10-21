3c3c51ac5ee5103bf79d4b8a9ef42f247d1b5c6f
Arun Kumar <arunkumarcs040@gmail.com>
2024-10-22 00:06:53 +0530

    backend new changes

---
+++ cookshaven/src/main/java/com/cookshaven/cookshaven/HomeController.java
@@ -0,0 +1,21 @@
+package com.cookshaven.cookshaven;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+@Controller
+public class HomeController {
+    @GetMapping("/home") 
+    public String showHomePage() {
+        return "index";
+    }
+
+    @GetMapping("/login")
+    public String showLoginPage(){
+        return "login";
+    }
+
+    @GetMapping("/order")
+    public String showOrderPage(){
+        return "orderpage";
+    }
+}
---
+++ cookshaven/src/test/java/com/cookshaven/cookshaven/CookshavenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cookshaven.cookshaven;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CookshavenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
