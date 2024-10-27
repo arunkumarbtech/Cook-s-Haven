5cb6e765948d120009c0c32ea68aac14e8757529
ASHEYYY13 <aswinsalomon4321@gmail.com>
2024-10-27 19:35:36 +0530

    fix:pushing the cart page

--- js/cart.js
+++ js/cart.js
@@ -1,6 +1,7 @@
 let basket =JSON.parse(localStorage.getItem("data")) || [];
-
-
+let cartBody = document.getElementById("carts");
+let cartTitle = document.getElementById("cart-title");
+let cartContainer = document.getElementById("cart-items-contain");
 let calculation =()=>{
     let cartCount = document.getElementById('cart-amount');
     cartCount.innerHTML =basket.map((x)=>x.item).reduce((x,y)=>x+y,0);
@@ -10,10 +11,41 @@ let calculation =()=>{
   calculation();
 let generateItems=()=>{
     if(basket.length !==0) {
-      
+    
+      cartContainer.innerHTML=`
+          <div id="cart-title" class="cart-title">           
+    <p>Items</p>
+     <p>Title</p>
+     <p>Price</p>
+     <p>Quantity</p>
+     <p>Total</p>
+     <p>Remove</p>
+          </div> 
+          <br>
+          <hr>
+      ${basket.map((x) => {
+    return `
+      <div id="generate-item">
+        
+      </div>
+    `;
+  }).join('')}
+
+      `;
+
     }
     else{
+      cartContainer.innerHTML =`
+         <h2 class="emptyText"> Cart is Empty</h2>
+         <div class = "button-wrapper">
+        <a href="orderpage.html">
+        <button class="orderBtn" >Place order</button>
+        </a>
+        </div>
+      `;
      
+     
+      
     }
 };
 generateItems();
